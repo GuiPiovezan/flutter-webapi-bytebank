a5cdfbd909a20f1231784a0c4557db43ed0e4ad7
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-06-25 20:56:23 -0300

    Adicionando interceptor http para exibir alguns determinados feedbacks como os headers, o conteúdo e o status code

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,8 +1,33 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print('Resquest');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+
+    return data;
+  }
+}
 
 var uri = Uri.parse('http://192.168.0.107:8080/transactions');
 
 void findAll() async {
-  final Response response = await get(uri);
-  print(response.body);
+  final Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()],
+  );
+  final Response response = await client.get(uri);
 }
